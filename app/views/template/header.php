4291b39f3b42bcde044e56b5102ebbb722133d4f
NuliaPratiwi <nuliapratiwi01@gmail.com>
2020-02-25 13:12:01 +0700

    tambah fitur denda dan crud img di add buku

--- app/views/template/header.php
+++ app/views/template/header.php
@@ -11,7 +11,7 @@
     <meta name="author" content="">
 
     <title><?= $data['judul'] ?> | SMK N 1 Denpasar</title>
-
+    <link href='<?= BASEURL ?>/img/asset/title.png' rel='shortcut icon'>
     <!-- Custom fonts for this template -->
     <link href="<?= BASEURL ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
