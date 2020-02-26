2c4b5bada7342be6d2dd821ee1ff748c4845aa1d
wahyupurnama <60025573+wahyupurnama43@users.noreply.github.com>
2020-02-26 13:05:56 +0800

    Tambah Database baru 26-02-2020

--- app/database/db_perpustakaan.sql
+++ app/database/db_perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Feb 2020 pada 06.57
+-- Waktu pembuatan: 26 Feb 2020 pada 02.46
 -- Versi server: 10.4.6-MariaDB
 -- Versi PHP: 7.3.9
 
@@ -44,7 +44,8 @@ CREATE TABLE `auth` (
 --
 
 INSERT INTO `auth` (`id_auth`, `nama`, `nis`, `kelas`, `username`, `password`, `id_level`, `id_jurusan`) VALUES
-(4, 'wahyu purnama', 27257, 'XII', 'wahyu', '$2y$10$zOXsSn5AGknqX0lmwl0yeu/XardR/ZKEybj7Ekq24efM/0jOFpCrK', 1, 2);
+(4, 'wahyu purnama', 27257, 'XII', 'wahyu', '$2y$10$zOXsSn5AGknqX0lmwl0yeu/XardR/ZKEybj7Ekq24efM/0jOFpCrK', 1, 1),
+(5, 'Yudha Adistara', 27259, 'XI', 'Yudha', '$2y$10$ZF.CO5CGqadKmXP00hjmnef3bByqmYefV3jN6cQS7vGxMoYKoWesO', 3, 5);
 
 -- --------------------------------------------------------
 
@@ -58,8 +59,16 @@ CREATE TABLE `tb_buku` (
   `pengarang` varchar(100) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `deskripsi` text NOT NULL,
-  `gambar` varchar(50) NOT NULL
+  `gambar` varchar(50) NOT NULL,
+  `status` tinyint(2) NOT NULL COMMENT '0=Dipinjam, 1=Ada'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`id_buku`, `nama_buku`, `pengarang`, `id_kategori`, `deskripsi`, `gambar`, `status`) VALUES
+(59, 'sad', 'adas', 2, 'asdas', 'b0.59715300 1582588942.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -78,7 +87,9 @@ CREATE TABLE `tb_jurusan` (
 
 INSERT INTO `tb_jurusan` (`id_jurusan`, `jurusan`) VALUES
 (1, 'Rekayasa Perangkat Lunak'),
-(2, 'Multimedia');
+(2, 'Multimedia'),
+(3, 'Teknik Komputer Jaringan'),
+(5, 'Teknik Kendaraan Ringan');
 
 -- --------------------------------------------------------
 
@@ -97,10 +108,10 @@ CREATE TABLE `tb_kategori` (
 --
 
 INSERT INTO `tb_kategori` (`id_kategori`, `kategori`, `kode`) VALUES
-(1, 'IT', '001'),
-(2, 'BIOLOGI', '002'),
+(1, 'PBO', '001'),
+(2, 'Biologi', '002'),
 (3, 'kimia', '003'),
-(4, 'fiksi', '004');
+(5, 'bahasa', '005');
 
 -- --------------------------------------------------------
 
@@ -121,6 +132,32 @@ INSERT INTO `tb_level` (`id_level`, `level`) VALUES
 (1, 'Admin'),
 (2, 'Petugas'),
 (3, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pinjam`
+--
+
+CREATE TABLE `tb_pinjam` (
+  `id_pinjam` int(11) NOT NULL,
+  `id_auth` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `id_jurusan` int(11) NOT NULL,
+  `tanggal_pinjam` date NOT NULL DEFAULT current_timestamp(),
+  `tanggal_kembali` date NOT NULL,
+  `lama_pinjam` int(11) NOT NULL,
+  `denda` int(11) NOT NULL,
+  `keadaan` tinyint(2) NOT NULL COMMENT '0=Dipinjam, 1=Ada'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_pinjam`
+--
+
+INSERT INTO `tb_pinjam` (`id_pinjam`, `id_auth`, `id_buku`, `id_jurusan`, `tanggal_pinjam`, `tanggal_kembali`, `lama_pinjam`, `denda`, `keadaan`) VALUES
+(87, 4, 59, 1, '2020-02-25', '2020-03-04', 8, 0, 1),
+(88, 4, 59, 1, '2020-02-25', '2020-03-06', 10, 4000, 1);
 
 --
 -- Indexes for dumped tables
@@ -160,6 +197,15 @@ ALTER TABLE `tb_level`
   ADD PRIMARY KEY (`id_level`);
 
 --
+-- Indeks untuk tabel `tb_pinjam`
+--
+ALTER TABLE `tb_pinjam`
+  ADD PRIMARY KEY (`id_pinjam`),
+  ADD KEY `id_auth` (`id_auth`),
+  ADD KEY `id_auth_2` (`id_auth`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -167,31 +213,37 @@ ALTER TABLE `tb_level`
 -- AUTO_INCREMENT untuk tabel `auth`
 --
 ALTER TABLE `auth`
-  MODIFY `id_auth` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_auth` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_buku`
 --
 ALTER TABLE `tb_buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_jurusan`
 --
 ALTER TABLE `tb_jurusan`
-  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_level`
 --
 ALTER TABLE `tb_level`
   MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pinjam`
+--
+ALTER TABLE `tb_pinjam`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -209,6 +261,13 @@ ALTER TABLE `auth`
 --
 ALTER TABLE `tb_buku`
   ADD CONSTRAINT `tb_buku_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `tb_kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_pinjam`
+--
+ALTER TABLE `tb_pinjam`
+  ADD CONSTRAINT `tb_pinjam_ibfk_1` FOREIGN KEY (`id_auth`) REFERENCES `auth` (`id_auth`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_pinjam_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `tb_buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
