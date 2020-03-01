d70d29686f0c7c83eaa9554bd3b3edb1b2c4754c
Dektra Gantenk <kp442863@gmail.com>
2020-03-01 16:58:40 +0800

    menambah file report

--- app/views/perpustakaan/laporan/index.php
+++ app/views/perpustakaan/laporan/index.php
@@ -1,4 +1,90 @@
+<!-- Begin Page Content -->
+        <div class="container-fluid">
 
-<a href="<?= BASEURL ?>/laporan/cetak">CETAK</a>
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+          	<div class=""></div>
+            <h1 class="h3 mb-0 text-gray-800">Generate Laporan &nbsp;<i class="fas fa-download text-gray-300"></i> </h1>
+            <div class=""></div>
+          </div>
 
-<h1>Hallo</h1>
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2 scale">
+              	<a href="" class="text-decoration-none">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-md font-weight-bold text-primary mb-1">Laporan Buku</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-download fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+                </a>
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-md font-weight-bold text-success mb-1">Laporan User</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-download fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-md font-weight-bold text-info mb-1">Laporan Peminjaman </div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"></div>
+                        </div>
+                        
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                     <i class="fas fa-download fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-md font-weight-bold text-warning mb-1">Laporan Pengembalian </div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-download fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
